c6e69cf87b816e17e1766fa9c878e94cdf39c8e7
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-04-27 00:46:48 +0300

    Add editorconfig file

--- LoanApplication.TacticalDdd/LoanApplication.TacticalDdd/DomainModel/Customer.cs
+++ LoanApplication.TacticalDdd/LoanApplication.TacticalDdd/DomainModel/Customer.cs
@@ -4,32 +4,36 @@ namespace LoanApplication.TacticalDdd.DomainModel;
 
 public class Customer : ValueObject<Customer>
 {
-    public NationalIdentifier NationalIdentifier { get;  }
+    public NationalIdentifier NationalIdentifier { get; }
     public Name Name { get; }
     public DateOnly Birthdate { get; }
     public MonetaryAmount MonthlyIncome { get; }
-    public Address Address { get;  }
+    public Address Address { get; }
 
     public Customer
     (
-        NationalIdentifier nationalIdentifier, 
-        Name name, 
-        DateOnly birthdate, 
-        MonetaryAmount monthlyIncome, 
+        NationalIdentifier nationalIdentifier,
+        Name name,
+        DateOnly birthdate,
+        MonetaryAmount monthlyIncome,
         Address address
     )
     {
-        if (nationalIdentifier==null)
+        if (nationalIdentifier == null)
             throw new ArgumentException("National identifier cannot be null");
-        if (name==null)
+
+        if (name == null)
             throw new ArgumentException("Name cannot be null");
-        if (monthlyIncome==null)
+
+        if (monthlyIncome == null)
             throw new ArgumentException("Monthly income cannot be null");
-        if (address==null)
+
+        if (address == null)
             throw new ArgumentException("Address cannot be null");
-        if (birthdate==default)
+
+        if (birthdate == default)
             throw new ArgumentException("Birthdate cannot be empty");
-            
+
         NationalIdentifier = nationalIdentifier;
         Name = name;
         Birthdate = birthdate;
--- LoanApplication.TacticalDdd/LoanApplication.TacticalDdd/DomainModel/LoanApplication.cs
+++ LoanApplication.TacticalDdd/LoanApplication.TacticalDdd/DomainModel/LoanApplication.cs
@@ -8,17 +8,17 @@ public class LoanApplication : Entity<LoanApplicationId>
     public LoanApplicationStatus Status { get; private set; }
     public ScoringResult Score { get; private set; }
     public Customer Customer { get; }
-    public Property Property { get;  }
+    public Property Property { get; }
     public Loan Loan { get; }
-        
+
     public Decision Decision { get; private set; }
 
     public Registration Registration { get; }
 
-        
+
     public LoanApplication(LoanApplicationNumber number, Customer customer, Property property, Loan loan, Operator registeredBy)
-        : this(number, LoanApplicationStatus.New, customer, property,loan, 
-            null,new Registration(SysTime.Today(), registeredBy), null)
+        : this(number, LoanApplicationStatus.New, customer, property, loan,
+            null, new Registration(SysTime.Today(), registeredBy), null)
     {
     }
 
@@ -47,16 +47,16 @@ public class LoanApplication : Entity<LoanApplicationId>
         {
             throw new ApplicationException("Cannot accept application before scoring");
         }
-            
+
         if (!decisionBy.CanAccept(this.Loan.LoanAmount))
         {
             throw new ApplicationException("Operator does not have required competence level to accept application");
         }
-            
+
         Status = LoanApplicationStatus.Accepted;
         Decision = new Decision(SysTime.Today(), decisionBy);
     }
-        
+
     public void Reject(Operator decisionBy)
     {
         if (Status != LoanApplicationStatus.New)
@@ -67,28 +67,28 @@ public class LoanApplication : Entity<LoanApplicationId>
         Status = LoanApplicationStatus.Rejected;
         Decision = new Decision(SysTime.Today(), decisionBy);
     }
-        
+
     private LoanApplication(
-        LoanApplicationNumber number, 
-        LoanApplicationStatus status, 
-        Customer customer, 
-        Property property, 
-        Loan loan, 
+        LoanApplicationNumber number,
+        LoanApplicationStatus status,
+        Customer customer,
+        Property property,
+        Loan loan,
         ScoringResult score,
         Registration registration,
         Decision decision)
     {
-        if (number==null)
+        if (number == null)
             throw new ArgumentException("Number cannot be null");
-        if (customer==null)
+        if (customer == null)
             throw new ArgumentException("Customer cannot be null");
-        if (property==null)
+        if (property == null)
             throw new ArgumentException("Property cannot be null");
-        if (loan==null)
+        if (loan == null)
             throw new ArgumentException("Loan cannot be null");
-        if (registration==null)
+        if (registration == null)
             throw new ArgumentException("Registration cannot be null");
-           
+
         Id = new LoanApplicationId(Guid.NewGuid());
         Number = number;
         Status = status;
@@ -100,7 +100,7 @@ public class LoanApplication : Entity<LoanApplicationId>
         Decision = decision;
     }
 }
-    
+
 public class LoanApplicationId : ValueObject<LoanApplicationId>
 {
     public Guid Value { get; }
@@ -119,7 +119,7 @@ public class LoanApplicationId : ValueObject<LoanApplicationId>
     {
     }
 }
-    
+
 public class LoanApplicationNumber : ValueObject<LoanApplicationNumber>
 {
     public string Number { get; }
@@ -129,14 +129,14 @@ public class LoanApplicationNumber : ValueObject<LoanApplicationNumber>
             throw new ArgumentException("Loan application number cannot be null or empty string");
         Number = number;
     }
-        
-        
+
+
     public static LoanApplicationNumber NewNumber() => new LoanApplicationNumber(Guid.NewGuid().ToString());
-        
+
     public static LoanApplicationNumber Of(string number) => new LoanApplicationNumber(number);
 
     public static implicit operator string(LoanApplicationNumber number) => number.Number;
-        
+
     protected override IEnumerable<object> GetAttributesToIncludeInEqualityCheck()
     {
         yield return Number;
